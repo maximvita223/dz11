9af1693fa46165718b17aa32b9b9e21058153839
Maxim <maks.bidenko.96@mail.ru>
2022-11-14 15:27:00 +0300

    Сделал 2 задание но неправильно или не до конца

---
+++ ex03.py
@@ -0,0 +1,14 @@
+# Задание 3.
+
+# Определить, какие из слов «attribute», «класс», «функция», «type»
+# невозможно записать в байтовом типе с помощью маркировки b'' (без encode decode).
+
+line1 = b'attribute'
+line2 = b'type'
+# line3 = b'класс'
+# line4 = b'функция'
+
+# File "c:\Users\HYPERPC\Desktop\das\Урок 1. Практическое задание\task_3.py", line 14
+#     var3 = b'класс'
+# File "c:\Users\HYPERPC\Desktop\das\Урок 1. Практическое задание\task_3.py", line 15
+#     var4 = b'функция'
